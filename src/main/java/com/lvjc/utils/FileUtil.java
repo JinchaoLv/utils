52de32abcba5dcc5f96fd49e2a47024e79535814
JinchaoLv <jinchao.lv@foxmail.com>
2017-11-02 19:20:16 +0800

    添加注释，更改版本号

--- src/main/java/com/lvjc/utils/FileUtil.java
+++ src/main/java/com/lvjc/utils/FileUtil.java
@@ -3,10 +3,16 @@ package com.lvjc.utils;
 import java.io.*;
 
 /**
+ * 文件流工具
  * Created by lvjc on 2017/11/2.
  */
 public class FileUtil {
 
+    /**
+     * 读取文件内容
+     * @param fileName
+     * @return
+     */
     public static String readFile(String fileName){
         File file = new File(fileName);
         StringBuilder builder = new StringBuilder();
@@ -21,14 +27,27 @@ public class FileUtil {
         return builder.toString();
     }
 
-    public static void writeFile(String content, String fileName) throws IOException {
+    /**
+     * 写入文件内容
+     * @param content 待写入的内容
+     * @param fileName 目标文件
+     * @param append 是否从末尾开始写入
+     * @throws IOException
+     */
+    public static void writeFile(String content, String fileName, boolean append) throws IOException {
         if(content == null)
             content = "";
-        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName)))){
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName, append)))){
             writer.write(content);
         }
     }
 
+    /**
+     * 复制文件内容
+     * @param sourceFile 源文件
+     * @param targetFile 目标文件
+     * @throws IOException
+     */
     public static void copyFile(String sourceFile, String targetFile) throws IOException {
         String content = readFile(sourceFile);
         if(content != null){
